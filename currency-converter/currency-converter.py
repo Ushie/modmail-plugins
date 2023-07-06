03e34a10ffee55d344cf50e7e9973eec066f02a7
Alexandre Teles (afterSt0rm) <alexandre.teles@ufba.br>
2023-07-06 11:59:51 -0300

    refactor: gotta go fast
    The `get_currency_exchange_rate` from the `ForeignExchange` class should be written as an async method so the network IO doesn't block the event loop. This rewrite would require `get_exchange_rate` to also be async, with only the `handle_api_call` method running sync (as it is entirely CPU bound).

--- currency-converter/currency-converter.py
+++ currency-converter/currency-converter.py
@@ -16,49 +16,76 @@ class CurrencyConverter(commands.Cog):
 
     @commands.command()
     @checks.has_permissions(PermissionLevel.REGULAR)
-    async def convert(self, ctx, amount: float = 1, from_currency: str.upper = "USD", to_currency: str.upper = "USD"):
-        # AlphaVantage has no API Key validation, any string that isn't "demo" will do
-        apiKey = token_urlsafe(10)
-        cc = ForeignExchange(key=apiKey)
-        try:
+    async def convert(
+        self,
+        ctx,
+        amount: float = 1,
+        from_currency: str = "USD",
+        to_currency: str = "USD",
+    ):
+        def get_exchange_rate(from_currency, to_currency):
+            apiKey = token_urlsafe(10)  # AlphaVantage has no API Key validation
+            cc = ForeignExchange(key=apiKey)
             data, _ = cc.get_currency_exchange_rate(
-                from_currency=from_currency, to_currency=to_currency)
-            exchange_rate = float(data['5. Exchange Rate'])
-            result = round(exchange_rate * amount, 3)
-            embed = Embed(
-                title="Currency Conversion Result",
-                description=f'The conversion of {amount:g} {from_currency} to {to_currency} is {result:g}',
-                color=self.bot.main_color,
+                from_currency=from_currency, to_currency=to_currency
             )
-            embed.set_footer(
-                text=f'Conversion Rate: 1 {from_currency} = {round(exchange_rate, 3):g} {to_currency}',
-                icon_url=self.bot.get_guild_icon(guild=ctx.guild),
-            )
-        except ValueError as e:
-            logger.debug(e)
-            if "https://www.alphavantage.co/premium/" in str(e):
-                embed = Embed(
-                    title="API Call Limit Exceeded",
-                    description="Apologies for the inconvenience. It seems that the maximum limit for API calls has been reached.",
-                    color=self.bot.error_color,
-                )
-            elif "Invalid API call" in str(e):
-                embed = Embed(
-                    title="Invalid Currency Conversion Request",
-                    description=f'The requested currency conversion from "{from_currency}" to "{to_currency}" is not valid. Please ensure that both source and target currencies are correctly specified using their respective currency codes.',
-                    color=self.bot.error_color,
-                )
-            else:
-                embed = Embed(
-                    title="Unexpected Error",
-                    description="An unexpected error occurred. Please open an issue at [GitHub](https://github.com/ushie/modmail-plugins) to report the problem.",
-                    color=self.bot.error_color,
-                )
-            embed.set_footer(
-                text=f'Please try again later',
-                icon_url=self.bot.get_guild_icon(guild=ctx.guild),
-            )
-        await ctx.send(embed=embed)
+            return {
+                "exchange_rate": float(data["5. Exchange Rate"]),
+                "from_currency": from_currency,
+                "to_currency": to_currency,
+            }
+
+        def handle_api_call(exchange_data):
+            match exchange_data["exchange_rate"]:
+                case None:
+                    logger.debug("API call limit exceeded")
+                    embed = Embed(
+                        title="API Call Limit Exceeded",
+                        description="Apologies for the inconvenience. It seems that the maximum limit for API calls has been reached.",
+                        color=self.bot.error_color,
+                    )
+                    embed.set_footer(
+                        text=f"Please try again later",
+                        icon_url=self.bot.get_guild_icon(guild=ctx.guild),
+                    )
+                case "Invalid API call":
+                    logger.debug("Invalid API call")
+                    embed = Embed(
+                        title="Invalid Currency Conversion Request",
+                        description=f'The requested currency conversion from "{exchange_data["from_currency"]}" to "{exchange_data["to_currency"]}" is not valid. Please ensure that both source and target currencies are correctly specified using their respective currency codes.',
+                        color=self.bot.error_color,
+                    )
+                    embed.set_footer(
+                        text=f"Please try again later",
+                        icon_url=self.bot.get_guild_icon(guild=ctx.guild),
+                    )
+                case float(_):
+                    result = round(exchange_data["exchange_rate"] * amount, 3)
+                    embed = Embed(
+                        title="Currency Conversion Result",
+                        description=f'The conversion of {amount:g} {exchange_data["from_currency"]} to {exchange_data["to_currency"]} is {result:g}',
+                        color=self.bot.main_color,
+                    )
+                    embed.set_footer(
+                        text=f'Conversion Rate: 1 {exchange_data["from_currency"]} = {round(exchange_data["exchange_rate"], 3):g} {exchange_data["to_currency"]}',
+                        icon_url=self.bot.get_guild_icon(guild=ctx.guild),
+                    )
+                case _:
+                    logger.debug("Unexpected error")
+                    embed = Embed(
+                        title="Unexpected Error",
+                        description="An unexpected error occurred. Please open an issue at [GitHub](https://github.com/ushie/modmail-plugins) to report the problem.",
+                        color=self.bot.error_color,
+                    )
+                    embed.set_footer(
+                        text=f"Please try again later",
+                        icon_url=self.bot.get_guild_icon(guild=ctx.guild),
+                    )
+            return embed
+
+        await ctx.send(
+            embed=handle_api_call(get_exchange_rate(from_currency, to_currency))
+        )
 
 
 async def setup(bot):
